5f98f482c4bbe3a7c9764a4fa9958329e0f4e3c4
Iris Michelsen <150297950+irismichelsen@users.noreply.github.com>
2024-05-11 17:32:20 -0700

    updating code

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -1,5 +1,4 @@
 // Store our API endpoint as queryUrl.
-//let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
 
 // Define a structure to use for the color gradient and depths
@@ -35,4 +34,42 @@ let depthColorBins = [
     label: "100+"
   },
 ];
+
+// GeoJSON format is a standard format for location information
+// Perform a GET request to the query URL.
+d3.json(queryUrl).then(function (data) {
+    console.log(data.features);
+    
+ // Using the features array sent back in the API data, create a GeoJSON layer, and add it to the map.
+
+  // Pass the features to a createFeatures() function:
+  createFeatures(data.features);
+
+});
+
+// Create a function to determine color based on value passed in
+// There are 10 different colors and 
+function getMarkerColor(value)
+{
+
+  // Initialize color to max depth
+  let color = depthColorBins[5].color;
+
+  //console.log(value, depthColorBins.length);
+
+  // Now check value passed to get correct color
+  for (let i=0; i < depthColorBins.length; i++) 
+  {
+    if (value < depthColorBins[i].maxDepth)
+    {
+      color = depthColorBins[i].color;
+      // Exit the loop because we have a match
+      break;
+    }
+  }
+
+  // if all else fails (haha), return last color
+  //console.log(color);
+  return color;
+};
 
